d81d0e7f8bd6145e0e1cd0cde74f4682e1ea8523
Thiago Tavares <thiagodevtavares@gmail.com>
2021-05-29 13:29:29 +0100

    strapi app created

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '37175400812152f1a3bff2320fb6623d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '90394a46-0a5d-4560-88da-d28d2853b795'
+};
